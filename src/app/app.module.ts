1bd188e8f5f7f5661b94a6a665978a719429bbf6
Tomas Larrain <tlarrain@kunder.cl>
2017-07-06 15:12:34 -0400

    faltaba subir algunos archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,39 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+import { Slides } from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp }           from './app.component';
+import { HomePage }        from '../pages/home/home';
+import { ComicsPage }      from '../pages/comics/comics';
+import { ComicDetailPage } from '../pages/comic-detail/comic-detail';
+import { ComicService }    from '../services/comic.service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ComicsPage,
+    ComicDetailPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ComicsPage,
+    ComicDetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ComicService,
+    Slides,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
