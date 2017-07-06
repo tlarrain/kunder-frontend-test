c3f7278d99327304c5bd140ea0454db839a98df8
Tomas Larrain <tlarrain@kunder.cl>
2017-07-06 15:39:21 -0400

    parece que faltaba la carpeta www en el repositorio

---
+++ www/build/1.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([1],{
+
+/***/ 267:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ComicDetailPageModule", function() { return ComicDetailPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(28);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__comic_detail__ = __webpack_require__(99);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var ComicDetailPageModule = (function () {
+    function ComicDetailPageModule() {
+    }
+    return ComicDetailPageModule;
+}());
+ComicDetailPageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__comic_detail__["a" /* ComicDetailPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__comic_detail__["a" /* ComicDetailPage */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__comic_detail__["a" /* ComicDetailPage */]
+        ]
+    })
+], ComicDetailPageModule);
+
+//# sourceMappingURL=comic-detail.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=1.main.js.map
